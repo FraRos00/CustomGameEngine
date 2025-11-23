f56fe1f431c5b615968f2dec39cf887e21555624
FraRos00 <101267161+FraRos00@users.noreply.github.com>
2025-11-23 12:49:29 +0100

    shaders wip

--- CustomEngine/include/animations/Animation.hpp
+++ CustomEngine/include/animations/Animation.hpp
@@ -2,37 +2,38 @@
 #include <raylib.h>
 #include <string>
 
-class Animation{
-    public:
-    Animation()=default;
-    Animation(std::string ssPath, int startX, int startY, int frameWidth, int frameHeight, float scale,
-              int frameCount, float frameSpeed=12.0f, bool loop = true);
-        
-    Animation(const Animation& other);
-    Animation& operator=(const Animation& other);
+class Animation {
+public:
+  Animation() = default;
+  Animation(std::string ssPath, int startX, int startY, int frameWidth,
+            int frameHeight, float scale, int frameCount,
+            float frameSpeed = 12.0f, bool loop = true);
 
-    Animation(Animation&& other)noexcept;
-    Animation& operator=(Animation&& other)noexcept;
+  Animation(const Animation &other);
+  Animation &operator=(const Animation &other);
 
-    void Update(float dt);
-    void Draw(Vector2 position, Color tint=WHITE)const;
-    void Reset();
-    bool IsFinished()const{return !loop && currentFrame>=frameCount;}
-    
-    ~Animation();
+  Animation(Animation &&other) noexcept;
+  Animation &operator=(Animation &&other) noexcept;
 
-    private:
-    std::string ssPath = "";
-    Texture2D *spritesheet = nullptr;
-    int frameCount = 0;
-    int currentFrame = 0;
-    float frameSpeed = 12.0f; //we want to animate at 12FPS
-    float timer = 0.0f;
-    int frameWidth = 0;
-    int frameHeight = 0;
-    float scale = 0.0f;
-    int startX = 0;
-    int startY = 0;
-    bool loop = true;
+  void Update(float dt);
+  void Draw(Vector2 position, Color tint = WHITE) const;
+  void Reset();
+  bool IsFinished() const { return !loop && currentFrame >= frameCount; }
+
+  ~Animation();
+
+private:
+  std::string ssPath = "";
+  Texture2D *spritesheet = nullptr;
+  Shader shader;
+  int frameCount = 0;
+  int currentFrame = 0;
+  float frameSpeed = 12.0f; // we want to animate at 12FPS
+  float timer = 0.0f;
+  int frameWidth = 0;
+  int frameHeight = 0;
+  float scale = 0.0f;
+  int startX = 0;
+  int startY = 0;
+  bool loop = true;
 };
-
--- CustomEngine/src/animations/Animation.cpp
+++ CustomEngine/src/animations/Animation.cpp
@@ -1,42 +1,44 @@
 #include "animations/Animation.hpp"
 #include "core/ResourceManager.hpp"
+#include <iostream>
+#include <raylib.h>
 
-Animation::Animation(std::string ssPath, int startX, int startY, int frameWidth, int frameHeight, float scale,
-              int frameCount, float frameSpeed, bool loop):ssPath(ssPath),startX(startX),startY(startY),frameWidth(frameWidth),
-              frameHeight(frameHeight),scale(scale),frameCount(frameCount),frameSpeed(frameSpeed),loop(loop){
-                ResourceManager &rm = ResourceManager::GetInstance();
-                rm.LoadTextureFromPath(ssPath);
-                spritesheet = rm.GetTexture(ssPath);
+Animation::Animation(std::string ssPath, int startX, int startY, int frameWidth,
+                     int frameHeight, float scale, int frameCount,
+                     float frameSpeed, bool loop)
+    : ssPath(ssPath), frameCount(frameCount), frameSpeed(frameSpeed),
+      frameWidth(frameWidth), frameHeight(frameHeight), scale(scale),
+      startX(startX), startY(startY), loop(loop) {
+  ResourceManager &rm = ResourceManager::GetInstance();
+  rm.LoadTextureFromPath(ssPath);
+  spritesheet = rm.GetTexture(ssPath);
+  // shader = LoadShader("src/shaders/color.vs", "src/shaders/color.fs");
+  // FIX: remove
+  std::cout << GetWorkingDirectory() << std::endl;
 }
 
 Animation::Animation(const Animation &other)
-    : ssPath(other.ssPath),
-      startX(other.startX),
-      startY(other.startY),
-      frameWidth(other.frameWidth),
-      frameHeight(other.frameHeight),
-      scale(other.scale),
-      frameCount(other.frameCount),
-      frameSpeed(other.frameSpeed),
-      loop(other.loop),
-      currentFrame(other.currentFrame),
-      timer(other.timer) {
+    : ssPath(other.ssPath), frameCount(other.frameCount),
+      currentFrame(other.currentFrame), frameSpeed(other.frameSpeed),
+      timer(other.timer), frameWidth(other.frameWidth),
+      frameHeight(other.frameHeight), scale(other.scale), startX(other.startX),
+      startY(other.startY), loop(other.loop) {
 
-    if (!ssPath.empty()) {
-        ResourceManager &rm = ResourceManager::GetInstance();
-        rm.LoadTextureFromPath(ssPath); // aumenta refcount
-        spritesheet = rm.GetTexture(ssPath);
-    }
+  if (!ssPath.empty()) {
+    ResourceManager &rm = ResourceManager::GetInstance();
+    rm.LoadTextureFromPath(ssPath); // aumenta refcount
+    spritesheet = rm.GetTexture(ssPath);
+  }
 }
 
-Animation& Animation::operator=(const Animation& other){
+Animation &Animation::operator=(const Animation &other) {
 
-    if(this!=&other){
-        //se avevo gia una texture caricata prima di copiare la rilascio
+  if (this != &other) {
+    // se avevo gia una texture caricata prima di copiare la rilascio
     if (!ssPath.empty())
-        ResourceManager::GetInstance().UnloadTextureFromPath(ssPath);
-    
-        // Copia dei dati
+      ResourceManager::GetInstance().UnloadTextureFromPath(ssPath);
+
+    // Copia dei dati
     ssPath = other.ssPath;
     startX = other.startX;
     startY = other.startY;
@@ -50,87 +52,85 @@ Animation& Animation::operator=(const Animation& other){
     timer = other.timer;
 
     if (!ssPath.empty()) {
-        ResourceManager &rm = ResourceManager::GetInstance();
-        rm.LoadTextureFromPath(ssPath);
-        spritesheet = rm.GetTexture(ssPath);
-    }else{
-        spritesheet=nullptr;
+      ResourceManager &rm = ResourceManager::GetInstance();
+      rm.LoadTextureFromPath(ssPath);
+      spritesheet = rm.GetTexture(ssPath);
+    } else {
+      spritesheet = nullptr;
     }
-
-    }
-    return *this;
+  }
+  return *this;
 }
 
 Animation::Animation(Animation &&other) noexcept
-    : ssPath(std::move(other.ssPath)),
-      spritesheet(other.spritesheet),
-      frameCount(other.frameCount),
-      currentFrame(other.currentFrame),
-      frameSpeed(other.frameSpeed),
-      timer(other.timer),
-      frameWidth(other.frameWidth),
-      frameHeight(other.frameHeight),
-      scale(other.scale),
-      startX(other.startX),
-      startY(other.startY),
+    : ssPath(std::move(other.ssPath)), spritesheet(other.spritesheet),
+      frameCount(other.frameCount), currentFrame(other.currentFrame),
+      frameSpeed(other.frameSpeed), timer(other.timer),
+      frameWidth(other.frameWidth), frameHeight(other.frameHeight),
+      scale(other.scale), startX(other.startX), startY(other.startY),
       loop(other.loop) {
 
-    other.spritesheet = nullptr;
-    other.ssPath.clear();
+  other.spritesheet = nullptr;
+  other.ssPath.clear();
 }
 
 Animation &Animation::operator=(Animation &&other) noexcept {
-    if (this == &other) return *this;
-
-    if (!ssPath.empty())
-        ResourceManager::GetInstance().UnloadTextureFromPath(ssPath);
-
-    ssPath = std::move(other.ssPath);
-    spritesheet = other.spritesheet;
-    frameCount = other.frameCount;
-    currentFrame = other.currentFrame;
-    frameSpeed = other.frameSpeed;
-    timer = other.timer;
-    frameWidth = other.frameWidth;
-    frameHeight = other.frameHeight;
-    scale = other.scale;
-    startX = other.startX;
-    startY = other.startY;
-    loop = other.loop;
-
-    other.spritesheet = nullptr;
-    other.ssPath.clear();
-
+  if (this == &other)
     return *this;
-}
 
-
-Animation::~Animation(){
+  if (!ssPath.empty())
     ResourceManager::GetInstance().UnloadTextureFromPath(ssPath);
+
+  ssPath = std::move(other.ssPath);
+  spritesheet = other.spritesheet;
+  frameCount = other.frameCount;
+  currentFrame = other.currentFrame;
+  frameSpeed = other.frameSpeed;
+  timer = other.timer;
+  frameWidth = other.frameWidth;
+  frameHeight = other.frameHeight;
+  scale = other.scale;
+  startX = other.startX;
+  startY = other.startY;
+  loop = other.loop;
+
+  other.spritesheet = nullptr;
+  other.ssPath.clear();
+
+  return *this;
 }
 
-void Animation::Update(float dt){
-    timer+=dt;
-    if(timer>=1.0f/frameSpeed){
-        timer=0.0f;
-        currentFrame = loop?(currentFrame+1)%frameCount:currentFrame+1;
-    }
+Animation::~Animation() {
+  ResourceManager::GetInstance().UnloadTextureFromPath(ssPath);
+  // UnloadShader(shader);
 }
 
-void Animation::Draw(Vector2 position, Color tint) const{
-    Rectangle dst{position.x , position.y , frameWidth*scale, frameHeight*scale};
-    if(spritesheet){
-        int frameX = startX+currentFrame*frameWidth;
-        Rectangle src{(float)frameX, (float)startY, (float)frameWidth, (float)frameHeight};
-        DrawTexturePro(*spritesheet, src, dst, {frameWidth * scale / 2, frameHeight * scale / 2}, 0.0f, tint );
-    }else{
-        DrawRectangleRec(dst,RED);
-    }
+void Animation::Update(float dt) {
+  timer += dt;
+  if (timer >= 1.0f / frameSpeed) {
+    timer = 0.0f;
+    currentFrame = loop ? (currentFrame + 1) % frameCount : currentFrame + 1;
+  }
 }
 
-
-void Animation::Reset(){
-    currentFrame=0;
-    timer=0.0f;
+void Animation::Draw(Vector2 position, Color tint) const {
+  Rectangle dst{position.x, position.y, frameWidth * scale,
+                frameHeight * scale};
+  if (spritesheet) {
+    int frameX = startX + currentFrame * frameWidth;
+    Rectangle src{(float)frameX, (float)startY, (float)frameWidth,
+                  (float)frameHeight};
+    // BeginShaderMode(shader);
+    DrawTexturePro(*spritesheet, src, dst,
+                   {frameWidth * scale / 2, frameHeight * scale / 2}, 0.0f,
+                   tint);
+    // EndShaderMode();
+  } else {
+    DrawRectangleRec(dst, RED);
+  }
 }
-    
+
+void Animation::Reset() {
+  currentFrame = 0;
+  timer = 0.0f;
+}
